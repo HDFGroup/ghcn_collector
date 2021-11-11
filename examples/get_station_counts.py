887b77425c82734975b1684461446264db8ac147
jreadey <jreadey@hdfgroup.org>
2021-11-11 07:19:31 -0800

    added logging

--- examples/get_station_counts.py
+++ examples/get_station_counts.py
@@ -1,15 +1,20 @@
 import sys
+import logging
 import h5pyd
  
 if len(sys.argv) < 2 or sys.argv[1] in ('-h', '--help'):
     print("usage: python get_station_ids <ghcn_file>")
     sys.exit(0)
 filename = sys.argv[1]
+
+logging.basicConfig(level=logging.INFO)
+
 f = h5pyd.File(filename, mode='r', use_cache=False)
 dset = f['data']
 station_year_map = {}
 cursor = dset.create_cursor()
 count = 0
+previous_year = 0
 for row in cursor:
     station_id = row['station_id'].decode('ascii')
     ymd = row['ymd'].decode('ascii')
@@ -18,6 +23,9 @@ for row in cursor:
         count += 1
         continue
     year = int(ymd[:4])  # format YYYYMMDD
+    if year != previous_year:
+        logging.info(f"got year: {year}")
+        previous_year = year
     if year not in station_year_map:
         station_year_map[year] = set()
     station_ids = station_year_map[year]
